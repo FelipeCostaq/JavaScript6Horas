d8f6e973c68b38a2ab76d381490732acc80c0b01
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-22 20:54:07 -0300

    sda

--- 004/obj.js
+++ 004/obj.js
@@ -1,1 +1,14 @@
-Object
+const carro = {
+    marca:'ford',
+    modelo:'ka',
+    ano:2015,
+    placa:'ABC-1234',
+    cor:'branca',
+    buzina: function(){console.log('Biiiiiiiiiii')},
+
+    completo: function(){
+        return `A marca é ${carro.marca} o modelo é ${carro.modelo} da cor ${carro.cor} com a placa ${carro.placa} do ano de ${carro.ano}`
+    }
+}
+
+console.log(`${carro.completo()}`)
