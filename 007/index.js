2e3068939327abe546a5565353d3d988625b16e7
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-28 11:50:02 -0300

    321

--- 007/index.js
+++ 007/index.js
@@ -1,3 +1,3 @@
-const lista = ['Felipe', 'Costa', '15', 'Estudante'];
-
-console.log(lista.join(' - '));
+const pessoa = ['Felipe', 'Costa', '15', 'Estudante'];
+pessoa.splice(1,0,'Item add1', 'item add2')
+console.log(pessoa.join(' - '));
