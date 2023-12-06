d4083b10450700d9f866284e24e960d1d0dd0138
Felipe Costa <felipecostasiqu@gmail.com>
2023-12-06 09:38:21 -0300

    45

--- 010/index.js
+++ 010/index.js
@@ -4,3 +4,12 @@ for (let i = ano; i >= 1900; i--){
     document.getElementById('ano').innerHTML +="<option value='" + i + "'>" +i+ "<option>"
     
 }
+
+
+const carros = ["Gol", "Fusca", "Brasília", "Del Rey", "Chevette"];
+var tamanho = carros.length;
+
+for(let i = 0; i < tamanho; i++){
+    document.getElementById('resultado').innerHTML += carros[i] + ` - `
+
+}
